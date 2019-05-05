cd8953ba93c734145f842a685a3c1be6c384b299
crndolo <ndoloraphael@gmail.com>
2019-05-05 12:36:31 +0300

    Product model created. This model stores infromation on all products available in our system. The model is then registered in admin.py

--- product/models.py
+++ product/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -12,6 +13,15 @@ def category_feature_image_name(instance, filename):
 	filename = '{}.{}'.format(current_time, ext)
 	return os.path.join('category_featued_image', filename)
 
+def product_feature_image_name(instance, filename):
+	#Get file expension
+	ext = filename.split('.')[-1]
+	#Getting current time
+	current_time = datetime.datetime.now()
+
+	filename = '{}.{}'.format(current_time, ext)
+	return os.path.join('product_featued_image', filename)
+
 class Category(models.Model):
 	category_name = models.CharField(_('Name'), max_length=255, unique=True, null=False, blank=False)
 	description = models.TextField(_('Description'), blank=True, null=True)
@@ -25,3 +35,23 @@ class Category(models.Model):
 
 	def __str__(self):
 		return self.category_name
+
+
+class Product(models.Model):
+	product_name = models.CharField(_('Name'), max_length=255, unique=True, blank=False, null=False)
+	category = models.OneToOneField(Category, on_delete=models.CASCADE)
+	brand = models.CharField(_('Brand'), max_length=255, unique=False, blank=False, null=False)
+	size = models.CharField(_('size'), max_length=255, unique=False, blank=False, null=False)
+	color = models.CharField(_('Color'), max_length=255, unique=False, blank=True, null=True)
+	description = models.TextField(_('Description'), blank=True, null=True)
+	image = models.ImageField(_('Featured Image - 100px x 100 px'), upload_to=product_feature_image_name, blank=True, null=True)
+
+	created_on = models.DateTimeField(_('Created On'), auto_now_add=True)
+	updated_on = models.DateTimeField(_('Updated On'), auto_now=True)
+
+	class Meta:
+		verbose_name = 'Product'
+		verbose_name_plural = 'Products'
+
+	def __str__(self):
+		return self.product_name
